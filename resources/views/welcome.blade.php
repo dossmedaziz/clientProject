0351df1e54806808c6bf45d5c1efa71d658699cc
dossmedaziz <dossaziz4@gmail.com>
2023-09-12 20:50:48 +0100

    fix the vendor assets

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -17,12 +17,12 @@
     {{--        <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Jost:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">--}}
 
     {{--        <!-- Vendor CSS Files -->--}}
-    <link href="{{resource_path('vendor/aos/aos.css')}}" rel="stylesheet">
-    <link href="{{resource_path('vendor/boxicons/css/boxicons.min.css')}}" rel="stylesheet">
-    <link href="{{resource_path('vendor/glightbox/css/glightbox.min.css')}}" rel="stylesheet">
-    <link href="{{resource_path('vendor/remixicon/remixicon.css')}}" rel="stylesheet">
-    <link href="{{resource_path('vendor/swiper/swiper-bundle.min.css')}}" rel="stylesheet">
-    <link href="{{resource_path('vendor/bootstrap-icons/bootstrap-icons.css')}}" rel="stylesheet">
+    <link href="{{asset('storage/vendor/aos/aos.css')}}" rel="stylesheet">
+    <link href="{{asset('storage/vendor/boxicons/css/boxicons.min.css')}}" rel="stylesheet">
+    <link href="{{asset('storage/vendor/glightbox/css/glightbox.min.css')}}" rel="stylesheet">
+    <link href="{{asset('storage/vendor/remixicon/remixicon.css')}}" rel="stylesheet">
+    <link href="{{asset('storage/vendor/swiper/swiper-bundle.min.css')}}" rel="stylesheet">
+    <link href="{{asset('storage/vendor/bootstrap-icons/bootstrap-icons.css')}}" rel="stylesheet">
 
 
     {{--        <!-- Template Main CSS File -->--}}
@@ -928,13 +928,13 @@
 
 <!-- Vendor JS Files -->
 
-<script src="{{resource_path('vendor/aos/aos.js')}}"></script>
-<script src="{{resource_path('vendor/glightbox/js/glightbox.min.js')}}"></script>
-<script src="{{resource_path('vendor/isotope-layout/isotope.pkgd.min.js')}}"></script>
-<script src="{{resource_path('vendor/swiper/swiper-bundle.min.js')}}"></script>
-<script src="{{resource_path('vendor/waypoints/noframework.waypoints.js')}}"></script>
-<script src="{{resource_path('vendor/php-email-form/validate.js')}}"></script>
-<script src="{{resource_path('vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+<script src="{{asset('storage/vendor/aos/aos.js')}}"></script>
+<script src="{{asset('storage/vendor/glightbox/js/glightbox.min.js')}}"></script>
+<script src="{{asset('storage/vendor/isotope-layout/isotope.pkgd.min.js')}}"></script>
+<script src="{{asset('storage/vendor/swiper/swiper-bundle.min.js')}}"></script>
+<script src="{{asset('storage/vendor/waypoints/noframework.waypoints.js')}}"></script>
+<script src="{{asset('storage/vendor/php-email-form/validate.js')}}"></script>
+<script src="{{asset('storage/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
 
 
 </body>
